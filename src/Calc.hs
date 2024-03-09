4ff0543fe9f71c41b73fd7f48edc68f5c6ff5ad9
Yutaka Nishimura <ytk.nishimura@gmail.com>
2024-03-10 08:40:34 +0900

    add stat

--- src/Calc.hs
+++ src/Calc.hs
@@ -9,5 +9,18 @@ normalise = T.concat . T.splitOn ","
 readXs :: String -> [Int]
 readXs = map (read . convertString . normalise . convertString) . words
 
-(.:) :: ([Int] -> a) -> String -> a
+-- (.:) :: ([Int] -> a) -> String -> a
 f .: a = f . readXs $ a
+
+avg :: [Int] -> Float
+avg xs = total / size 
+  where
+    total = fromIntegral . sum $ xs
+    size = fromIntegral . length $ xs
+
+avg' :: [Int] -> Int
+avg' xs = total `div` size 
+  where
+    total = sum xs
+    size = length xs
+
